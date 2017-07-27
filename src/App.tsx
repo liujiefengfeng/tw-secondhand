c09d6518283b8b384c04aea53d0298adc67668ee
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-07-27 23:59:29 +0800

    [ jiefeng ] use hash history instead of browser history.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/createHashHistory';
 import { ConnectedRouter } from 'react-router-redux';
 
 import storeConfigure from './store';
 import routers from './store/routers';
 
-const history = createHistory();
+const history = createHistory({
+    basename: 'tw-secondhand'
+});
+
 const store = storeConfigure(history);
 
 export default () => (
