1cc0eb42a4eabeec57a91d6e52037adb12cbca39
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-07-29 09:51:26 +0800

    [ jiefeng ] make ButtonWithColor component can receive isGreyButton props, to make the color to grey, and default color is yellow.

--- src/components/ButtonWithColor/ButtonWithColor.test.tsx
+++ src/components/ButtonWithColor/ButtonWithColor.test.tsx
@@ -2,8 +2,16 @@ import * as React from 'react';
 import ButtonWithColor from './ButtonWithColor';
 import { shallow } from 'enzyme';
 
-it('ButtonWithColor should render with customize text', () => {
-    const buttonWithColor = shallow(<ButtonWithColor buttonContent={'buttonText'}/>);
-
+describe('ButtonWithColor', () => {
+  let buttonWithColor;
+  beforeEach(() => {
+    buttonWithColor = shallow(<ButtonWithColor buttonContent={'buttonText'} isGreyButton={true}/>);
+  });
+  it('ButtonWithColor should render with customize text', () => {
     expect(buttonWithColor.find('button').text()).toEqual('buttonText');
+  });
+  
+  it('ButtonWithColor should render with grey color by props isGreyButton', () => {
+    expect(buttonWithColor.find('button').props()).toHaveProperty('className', 'button-grey');
+  });
 });
