f2b21479c700dff6187c36014853d9b8469f52aa
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-08-05 13:46:03 +0800

    [ jiefeng ] add fetch to call the real api.

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,4 +1,21 @@
 import * as D from '../definitions';
-import { fetchJson } from './utils';
 
-export const login = (user: D.UserForLogin): Promise<D.User> => fetchJson('/login', user);
+export const login = (user: D.UserForLogin): Promise<D.User> => {
+  const response = fetch('http://secondhand.leanapp.cn/users/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: user.username,
+      password: user.password
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  
+  return response.then((Response) => {
+    if (Response.status === 200) {
+      return Response.json();
+    }
+    return null;
+  });
+};
