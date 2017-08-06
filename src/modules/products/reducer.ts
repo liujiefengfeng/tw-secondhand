8f4552ecfc0cf3d06507fcf509d370c3333171cf
JellyLu <634725546@qq.com>
2017-08-06 16:26:53 +0800

    [Yangjie] - add logic in products reducer

--- src/modules/products/reducer.ts
+++ src/modules/products/reducer.ts
@@ -7,7 +7,9 @@ const productsReducer: Redux.Reducer<D.ProductsState> =
   (state: D.ProductsState, action: D.ProductsAction): D.ProductsState => {
   state = state || initialState;
   switch (action.type) {
-    case 'BOUGHT_PRODUCTS_SUC': {
+    case 'BOUGHT_PRODUCTS_SUC':
+    case 'SOLD_PRODUCTS_SUC':
+    {
       return action.success;
     }
     default:
