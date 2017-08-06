b3de71843cbf5f50bf14a5cc053d68d63129c643
JellyLu <634725546@qq.com>
2017-08-06 23:59:35 +0800

    [Yangjie] - Add page sold products.

--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -4,11 +4,13 @@ import { Route } from 'react-router';
 import HomePage from '../containers/pages/Home/HomePage';
 import AboutUsPage from '../containers/pages/AboutUs/AboutUsPage';
 import BoughtProductsPage from '../containers/pages/BoughtProductPage/BoughtProductPage';
+import SoldProductsPage from '../containers/pages/SoldProductsPage/SoldProductsPage';
 
 const router = (
     <div>
         <Route exact={true} path="/" component={HomePage}/>
         <Route path="/mine/bought-products" component={BoughtProductsPage}/>
+        <Route path="/mine/sold-products" component={SoldProductsPage}/>
         <Route path="/about-us" component={AboutUsPage}/>
     </div>
 );
