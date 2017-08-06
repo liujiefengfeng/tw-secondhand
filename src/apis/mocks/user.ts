9f8507539818f0abe6598f92469650a53306ba4c
JellyLu <634725546@qq.com>
2017-08-06 15:50:50 +0800

    [Yangjie] - Add session token to mock data user login api.

--- src/apis/mocks/user.ts
+++ src/apis/mocks/user.ts
@@ -1,3 +1,3 @@
 import * as fetchMock from 'fetch-mock';
 
-fetchMock.mock('http://secondhand.leanapp.cn/users/login', {username: 'Tom'}, {method: 'POST'});
+fetchMock.mock('http://secondhand.leanapp.cn/users/login', {username: 'Tom', sessionToken: '0shwjuon5x5bitvhy7wa27nt8'}, {method: 'POST'});
