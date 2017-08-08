2e56664891c72b30d7f7ad9a2db9a1928a148cfe
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-08-09 01:01:51 +0800

    [ jiefeng ] rename image to img. just keep one name in the project.

--- src/components/ProductDetailsPopup/ProductDetailsPopup.tsx
+++ src/components/ProductDetailsPopup/ProductDetailsPopup.tsx
@@ -1,14 +1,17 @@
 const React = require('react');
 import Header from '../Header/Header';
 import * as P from '../ProductDetail/ProdcutDetail';
+import * as classNames from 'classnames';
+import './ProductDetailsPopup.css';
 
 interface ProductDetailsPopupProps {
   onIconClick?: () => void;
+  isActive?: boolean;
   details: P.ProductDetailProps;
 }
 
 export default (props: ProductDetailsPopupProps) => (
-  <div className="product-details-popup">
+  <div className={classNames('product-details-popup', {'hidden-product-details-popup': !props.isActive})}>
     <Header closeIcon={true} headerContext="商品详情" onClick={props.onIconClick}/>
     <P.ProductDetail {...props.details} />
   </div>
