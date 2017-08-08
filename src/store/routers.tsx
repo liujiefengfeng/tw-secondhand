e189b5882339baa5bccb882e00a8ca4e4c01ca0e
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-08-08 15:22:14 +0800

    [ jiefeng ] add my profile to the router.

--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -3,12 +3,14 @@ import { Route } from 'react-router';
 
 import HomePage from '../containers/pages/Home/HomePage';
 import AboutUsPage from '../containers/pages/AboutUs/AboutUsPage';
+import MyProfilePage from '../containers/pages/MyProfile/MyProfile';
 import BoughtProductsPage from '../containers/pages/BoughtProductPage/BoughtProductPage';
 import SoldProductsPage from '../containers/pages/SoldProductsPage/SoldProductsPage';
 
 const router = (
     <div>
         <Route exact={true} path="/" component={HomePage}/>
+        <Route path="/my-profile" component={MyProfilePage}/>
         <Route path="/my-profile/bought-products" component={BoughtProductsPage}/>
         <Route path="/my-profile/sold-out" component={SoldProductsPage}/>
         <Route path="/about-us" component={AboutUsPage}/>
